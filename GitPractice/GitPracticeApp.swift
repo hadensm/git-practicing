ee984e5beff081ece7e54d42e0767fc21732fbb2
hadensm <152567682+hadensm@users.noreply.github.com>
2024-11-07 15:37:48 +0900

    First Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Haden Judd on 2024/11/07.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
